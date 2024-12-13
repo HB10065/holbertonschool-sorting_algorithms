6cf5df95196f3f783145379f18f38d4942e1394a
HB10065 <10065@holbertonstudents.com>
2024-12-13 17:42:50 +0000

    arreglo \0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,20 +6,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t place, i = 0;
+	size_t place, print = size;
 	int remp;
 
-	while (i < size)
+	while (size != 0)
 	{
-		for (place = 0; place < size; place++)
+		for (place = 0; place < size - 1; place++)
 		{
 			if (array[place] > array[place + 1])
 			{
 				remp = array[place];
 				array[place] = array[place + 1];
 				array[place + 1] = remp;
+
+				print_array(array, print);
 			}
 		}
-		i++;
+		size--;
 	}
 }
