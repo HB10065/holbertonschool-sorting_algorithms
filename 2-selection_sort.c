730f197fcc8212ec6f10df7d76838975a63c5c39
root <brudossant@gmail.com>
2024-12-15 23:18:49 +0000

    correccion ej 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,20 +8,32 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t a = 0, b = (size - 1), c, temp;
+	size_t a, c, min_idx, temp;
 
-	while (a != b)
+	if (!array || size < 2)
+		return;
+
+	a = 0;
+
+	while (a < size - 1)
 	{
+		min_idx = a;
+
 		for (c = a + 1; c < size; c++)
 		{
-			if (array[a] > array[c])
-			{
-				temp = array[a];
-				array[a] = array[c];
-				array[c] = temp;
-				print_array(array, size);
-			}
+			if (array[c] < array[min_idx])
+				min_idx = c;
 		}
+
+		if (min_idx != a)
+		{
+			temp = array[a];
+			array[a] = array[min_idx];
+			array[min_idx] = temp;
+
+			print_array(array, size);
+		}
+
 		a++;
 	}
 }
