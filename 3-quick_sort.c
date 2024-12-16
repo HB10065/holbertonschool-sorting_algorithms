58181646ab96c81d0b4705c262e5111c14d817f2
BruDosSant <brudossant@gmail.com>
2024-12-15 21:54:28 -0300

    desesperado y con comentarios de gpt

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using Quick Sort
- * @array: Pointer to the array
- * @size: Size of the array
+ * quick_sort - Ordena un arreglo de enteros en orden ascendente usando Quick Sort
+ * @array: Puntero al arreglo
+ * @size: Tamaño del arreglo
  */
 void quick_sort(int *array, size_t size)
 {
@@ -14,39 +14,59 @@ void quick_sort(int *array, size_t size)
 }
 
 /**
- * quick_sort_recursive - hace el swap
- * @array: pointer al array inicio del array
- * @inicio: inicio del array
- * @end: final del array
- * @size: tamaño del array
+ * quick_sort_recursive - Ordena recursivamente el arreglo usando Quick Sort
+ * @array: Puntero al arreglo
+ * @inicio: Índice inicial de la partición
+ * @end: Índice final de la partición
+ * @size: Tamaño del arreglo
  */
 void quick_sort_recursive(int *array, int inicio, int end, size_t size)
 {
-	int i, j, pivot, temp;
+	int pivot_index;
 
-	if (inicio >= end)
-		return;
+	if (inicio < end)
+	{
+		pivot_index = lomuto_partition(array, inicio, end, size);
+		quick_sort_recursive(array, inicio, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, end, size);
+	}
+}
 
-	pivot = array[end];
-	i = inicio - 1;
+/**
+ * lomuto_partition - Particiona el arreglo usando el esquema de Lomuto
+ * @array: Puntero al arreglo
+ * @inicio: Índice inicial de la partición
+ * @end: Índice final de la partición
+ * @size: Tamaño del arreglo
+ * Return: La posición final del pivote
+ */
+int lomuto_partition(int *array, int inicio, int end, size_t size)
+{
+	int pivot = array[end];
+	int i = inicio - 1, j, temp;
 
 	for (j = inicio; j < end; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 		}
 	}
 
-	temp = array[i + 1];
-	array[i + 1] = array[end];
-	array[end] = temp;
+	if (array[i + 1] != array[end])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
 
-	print_array(array, size);
-
-	quick_sort_recursive(array, inicio, i, size);
-	quick_sort_recursive(array, i + 2, end, size);
+	return (i + 1);
 }
